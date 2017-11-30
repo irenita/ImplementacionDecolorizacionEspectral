76990c8142e83ef22fed7c7634e83cb8dee76843
Irena Cabanach Dresden <iresaku@hotmail.com>
2017-11-30 13:34:50 -0400

    Add project files.

---
+++ Prueba1/Def_controles.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef DEF_CONTROLES_   // if controles hasn't been included yet...
+#define DEF_CONTROLES_
+
+#ifndef DEF_IOSTREAM_ 
+#include <iostream>
+using namespace std;
+#endif
+
+
+void ControlImprimir();
+void CargarImagenBGR();
+
+
+#endif
---
+++ Prueba1/controles.cpp
@@ -0,0 +1,19 @@
+
+#ifndef CONTROLES_   // if controles hasn't been included yet...
+#define CONTROLES_
+
+#include "Def_controles.h"
+
+void ControlImprimir() {
+	cout << "\nLlamando a un control";
+}
+
+
+void CargarImagenBGR() {
+	cout << "\nCargando la imagen BGR";
+
+}
+
+
+
+#endif
